95d56b0b89e5303162954dd1509f7e1c82890a26
adam-weiler <adam.weiler@yahoo.ca>
2019-07-16 22:23:18 -0400

    Parsing file, disabled warnings.

--- test.py
+++ test.py
@@ -1,2 +1,18 @@
+# -*- coding: utf-8 -*-
 import cssutils
 import webcolors
+
+import logging
+cssutils.log.setLevel(logging.CRITICAL)
+
+
+# print(webcolors.name_to_hex(u'goldenrod'))
+
+
+
+
+parser = cssutils.CSSParser()
+# optionally
+parser.setFetcher('none')
+sheet = parser.parseFile('main.css', 'ascii')
+print(sheet.cssText)
