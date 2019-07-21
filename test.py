fa9c0ea8cc3e5da6221ca7270260e6ee39884875
adam-weiler <adam.weiler@yahoo.ca>
2019-07-21 13:16:13 -0400

    Rewriting the If statement.

--- test.py
+++ test.py
@@ -10,13 +10,12 @@ cssutils.log.setLevel(logging.CRITICAL)
 
 
 
-
 parser = cssutils.CSSParser()
 # optionally
 parser.setFetcher('none')
 
 
-sheet = parser.parseFile('main.css', 'ascii')
+sheet = parser.parseFile('other.css', 'ascii')
 # sheet = parser.parseFile('other.css', 'ascii')
 
 for rule in sheet:
@@ -24,25 +23,30 @@ for rule in sheet:
         # find property
         for property in rule.style:
             if property.name == 'color' or property.name == 'background-color':
+                # print(f'Hey {property.value[0]}')
 
-                if property.value.startswith( '#' ):
-                    pass
+                # if property.value.startswith( '#' ):
+                #     pass
                     # print('This is a #FFF')
-                elif property.value.startswith( 'rgba' ):
-                    pass
-                    # print(f'This is a rgba {property.value}')
-                    # property.value = webcolors.rgb_to_hex(property.value)
-                elif property.value.startswith ('transparent') or property.value.startswith ('inherit'):
+
+                if property.value[0] != '#' and 'rgb' not in property.value and 'transparent' not in property.value and 'inherit' not in property.value:
+                    print(f'This is a named value # - {property.value}\n')
+                    property.value = webcolors.name_to_hex(property.value)
                     pass
                 else:
-                    print('This is a color name')
-                    property.value = webcolors.name_to_hex(property.value)
+                    print(f'This is a #, or rgb, or transparent, or inherit - {property.value}\n')
 
 
-
-                # print(property.value)
-                # property.value = webcolors.name_to_hex(property.value)
-                # break
+                # elif property.value.startswith( 'rgba' ):
+                #     pass
+                #     # print(f'This is a rgba {property.value}')
+                #     # property.value = webcolors.rgb_to_hex(property.value)
+                # elif property.value.startswith ('transparent') or property.value.startswith ('inherit'):
+                #     pass
+                # else:
+                #     # print('This is a color name')
+                #     # property.value = webcolors.name_to_hex(property.value)
+                #     pass
 
 
 
