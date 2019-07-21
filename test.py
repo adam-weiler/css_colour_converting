3e47e5d6240d56f994d80065c0fa7a9c35516714
adam-weiler <adam.weiler@yahoo.ca>
2019-07-21 14:06:36 -0400

    Project complete, cleaning up code.

--- test.py
+++ test.py
@@ -3,52 +3,29 @@ import cssutils
 import webcolors
 
 import logging
-cssutils.log.setLevel(logging.CRITICAL)
+cssutils.log.setLevel(logging.CRITICAL) #Disables warnings and errors triggered by new CSS properties and values.
 
-
-# print(webcolors.name_to_hex(u'goldenrod'))
-
-
-
-parser = cssutils.CSSParser()
+# parser = cssutils.CSSParser()
 # optionally
-parser.setFetcher('none')
+# parser.setFetcher('none') #The default fetcher emits a warning if encountering a different mimetype. Callin setFetcher('none') resets cssutils to use its default function. And is not needed?
+# sheet = parser.parseFile('main.css') #New call with parser.
 
-
-sheet = parser.parseFile('other.css', 'ascii')
-# sheet = parser.parseFile('other.css', 'ascii')
+sheet = cssutils.parseFile('main.css') #Original call without parser.
 
 for rule in sheet:
     if rule.type == rule.STYLE_RULE:
-        # find property
-        for property in rule.style:
+        for property in rule.style: # Find property
             if property.name == 'color' or property.name == 'background-color':
-                # print(f'Hey {property.value[0]}')
-
-                # if property.value.startswith( '#' ):
-                #     pass
-                    # print('This is a #FFF')
-
-                if property.value[0] != '#' and 'rgb' not in property.value and 'transparent' not in property.value and 'inherit' not in property.value:
-                    print(f'This is a named value # - {property.value}\n')
+                if property.value[0] != '#' and 'rgb' not in property.value and 'transparent' not in property.value and 'inherit' not in property.value: #Checks if value is a named colour.
+                    # print(f'This is a named value: {property.value}')
                     property.value = webcolors.name_to_hex(property.value)
+                    # print(f'Replace with: {property.value}\n')
+                else: #Else, property is not a named colour.
+                    # print(f'This is a #, or rgb, or transparent, or inherit value: {property.value}')
+                    # print('It will not be replaced.\n')
                     pass
-                else:
-                    print(f'This is a #, or rgb, or transparent, or inherit - {property.value}\n')
 
+with open('output.css', 'wb') as f:
+    f.write(sheet.cssText)
 
-                # elif property.value.startswith( 'rgba' ):
-                #     pass
-                #     # print(f'This is a rgba {property.value}')
-                #     # property.value = webcolors.rgb_to_hex(property.value)
-                # elif property.value.startswith ('transparent') or property.value.startswith ('inherit'):
-                #     pass
-                # else:
-                #     # print('This is a color name')
-                #     # property.value = webcolors.name_to_hex(property.value)
-                #     pass
-
-
-
-
-print(sheet.cssText)
+print('The new CSS file is output.css.')
